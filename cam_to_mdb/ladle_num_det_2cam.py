12cdd8f46bb09cc4d2b774c6a2ac44af0ac1cecc
PrudhviGudla <gvssprudhvi@kgpian.iitkgp.ac.in>
2024-08-13 18:17:13 +0530

    .

--- cam_to_mdb/ladle_num_det_2cam.py
+++ cam_to_mdb/ladle_num_det_2cam.py
@@ -118,7 +118,7 @@ def model(img,ct):
         return img
 
 # cap = cv2.VideoCapture('rtsp://adminp:adminp@192.168.0.2:554/stream1')
-cap1=cv2.VideoCapture('rtsp://adminp:adminp@192.168.0.2:554/stream1')
+cap1 = cv2.VideoCapture('rtsp://adminp:adminp@192.168.0.2:554/stream1')
 cap2 = cv2.VideoCapture('rtsp://adminp:adminp@192.168.0.3:554/stream1')
 
 ct = 1
